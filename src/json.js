bb679200f4c5c0f1a7e89248e5e567acee72df50
AliHashemi2000 <111985802+AliHashemi2000@users.noreply.github.com>
2023-05-25 15:59:08 +0100

    Initial commit

---
+++ src/json.js
@@ -0,0 +1,253 @@
+export const json = {
+ "title": "Diagnostic Tool ++",
+ "description": "This is the survey part of the Diagnostic Tool",
+ "logoPosition": "right",
+ "pages": [
+  {
+   "name": "page1",
+   "elements": [
+    {
+     "type": "radiogroup",
+     "name": "question1",
+     "title": "Scenario 1: Suppose you have been working as a shift manager in “Ada pizzas” for a few years. They provide eat in and delivery. They also get customers through Just Eat and Uber Eats. You have heard that they are thinking of using spreadsheets to records ales data and management information. Would you be interested in using spreadsheets for your work?",
+     "choices": [
+      {
+       "value": "Item 1",
+       "text": "Not Interested"
+      },
+      {
+       "value": "Item 2",
+       "text": "Not Sure"
+      },
+      {
+       "value": "Item 3",
+       "text": "Sightly Interested"
+      },
+      {
+       "value": "Item 4",
+       "text": "Interested"
+      },
+      {
+       "value": "Item 5",
+       "text": "Highly Interested"
+      }
+     ]
+    },
+    {
+     "type": "radiogroup",
+     "name": "question2",
+     "title": "Scenario 2: Suppose you are a shift runner at a coffee shop, and you have been given a few target numbers to hit in terms of sales, customer feedbacks, costs of sale and labour during your shift. You have come to know about the use of spreadsheets in managing data. Would you be interested in using tools for keeping record of this data?",
+     "choices": [
+      {
+       "value": "Item 1",
+       "text": "Not Interested"
+      },
+      {
+       "value": "Item 2",
+       "text": "Not Sure"
+      },
+      {
+       "value": "Item 3",
+       "text": "Slightly Interested"
+      },
+      {
+       "value": "Item 4",
+       "text": "Interested"
+      },
+      {
+       "value": "Item 5",
+       "text": "Highly Interested"
+      }
+     ]
+    },
+    {
+     "type": "radiogroup",
+     "name": "question3",
+     "title": "Scenario 3: Suppose you are a director of a marketing firm, and you are already making use of spreadsheets in creating budget plans for your company. Every Marketer loves data! as a marketing firm, would you like to learn and look for ways to better understand Key Performance Indicators (KPI’s) and keep a track of Client’s feedback through spreadsheets?",
+     "choices": [
+      {
+       "value": "Item 1",
+       "text": "Not Interested"
+      },
+      {
+       "value": "Item 2",
+       "text": "Not Sure"
+      },
+      {
+       "value": "Item 4",
+       "text": "Slightly Interested"
+      },
+      {
+       "value": "Item 3",
+       "text": "Interested"
+      },
+      {
+       "value": "Item 5",
+       "text": "Highly Interested"
+      }
+     ]
+    }
+   ],
+   "title": "Information Management"
+  },
+  {
+   "name": "page2",
+   "elements": [
+    {
+     "type": "radiogroup",
+     "name": "question4",
+     "title": "Scenario 1: Suppose you are a shift runner at Ali’s chicken. A customer rings at the restaurant and is complaining about a missing item from a recent delivery. He is mad and is shouting at one of your reception staff. Would you be interested in taking up that phone call from your colleague?",
+     "choices": [
+      {
+       "value": "Item 1",
+       "text": "Not Interested"
+      },
+      {
+       "value": "Item 2",
+       "text": "Not sure"
+      },
+      {
+       "value": "Item 3",
+       "text": "Slightly Interested"
+      },
+      {
+       "value": "Item 4",
+       "text": "Interested"
+      },
+      {
+       "value": "Item 5",
+       "text": "Highly Interested"
+      }
+     ]
+    },
+    {
+     "type": "radiogroup",
+     "name": "question5",
+     "title": "Scenario 2: Imagine yourself working in a car garage. A recent customer shows up with a complain that his car is not fixed properly. He is quite angry, and he wants his money to be refunded. Would you like to take part in resolving this customer’s complaints?",
+     "choices": [
+      {
+       "value": "Item 1",
+       "text": "Not Interested"
+      },
+      {
+       "value": "Item 2",
+       "text": "Not Sure"
+      },
+      {
+       "value": "Item 3",
+       "text": "Slightly Interested"
+      },
+      {
+       "value": "Item 4",
+       "text": "Interested"
+      },
+      {
+       "value": "Item 5",
+       "text": "Highly Interested"
+      }
+     ]
+    },
+    {
+     "type": "radiogroup",
+     "name": "question6",
+     "title": "Scenario 3: Suppose you lead a customer experience team at a car garage. One of your customer rings the garage and starts yelling on one of your team members. As a result, your team member hand over the phone to you. Would you be interested in this type of customer facing or this is something that does not excites you much?",
+     "choices": [
+      {
+       "value": "Item 1",
+       "text": "Not Interested"
+      },
+      {
+       "value": "Item 2",
+       "text": "Not sure"
+      },
+      {
+       "value": "Item 3",
+       "text": "Slightly Interested"
+      },
+      {
+       "value": "Item 4",
+       "text": "Interested"
+      },
+      {
+       "value": "Item 5",
+       "text": "Highly Interested"
+      }
+     ]
+    }
+   ],
+   "title": "Customer Experience"
+  },
+  {
+   "name": "page3",
+   "elements": [
+    {
+     "type": "radiogroup",
+     "name": "question7",
+     "title": "Scenario 1:",
+     "choices": [
+      "Item 1",
+      "Item 2",
+      "Item 3"
+     ]
+    },
+    {
+     "type": "radiogroup",
+     "name": "question8",
+     "title": "Scenario 2:",
+     "choices": [
+      "Item 1",
+      "Item 2",
+      "Item 3"
+     ]
+    },
+    {
+     "type": "radiogroup",
+     "name": "question9",
+     "title": "Scenario 3:",
+     "choices": [
+      "Item 1",
+      "Item 2",
+      "Item 3"
+     ]
+    }
+   ],
+   "title": "People Management"
+  },
+  {
+   "name": "page4",
+   "elements": [
+    {
+     "type": "radiogroup",
+     "name": "question10",
+     "title": "Scenario 1:",
+     "choices": [
+      "Item 1",
+      "Item 2",
+      "Item 3"
+     ]
+    },
+    {
+     "type": "radiogroup",
+     "name": "question11",
+     "title": "Scenario 2:",
+     "choices": [
+      "Item 1",
+      "Item 2",
+      "Item 3"
+     ]
+    },
+    {
+     "type": "radiogroup",
+     "name": "question12",
+     "title": "Scenario 3:",
+     "choices": [
+      "Item 1",
+      "Item 2",
+      "Item 3"
+     ]
+    }
+   ],
+   "title": "Financial Management"
+  }
+ ]
+}
